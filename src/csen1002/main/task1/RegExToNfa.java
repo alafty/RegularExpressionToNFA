23384ee4f2b29f017e79e9aaa7b81115baf647cc
alafty <mohamedelalafty@yahoo.com>
2024-02-15 23:24:44 +0200

    Added Template File

---
+++ src/csen1002/main/task1/RegExToNfa.java
@@ -0,0 +1,34 @@
+package csen1002.main.task1;
+
+/**
+ * Write your info here
+ * 
+ * @name Jane Smith
+ * @id 49-0234
+ * @labNumber 06
+ */
+
+public class RegExToNfa {
+
+	/**
+	 * Constructs an NFA corresponding to a regular expression based on Thompson's
+	 * construction
+	 * 
+	 * @param input The alphabet and the regular expression in postfix notation for
+	 *              which the NFA is to be constructed
+	 */
+	public RegExToNfa(String input) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return Returns a formatted string representation of the NFA. The string
+	 *         representation follows the one in the task description
+	 */
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
